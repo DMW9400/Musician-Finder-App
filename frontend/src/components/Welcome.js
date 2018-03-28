93986d9e7fbd5387f6692a3237e3664c3fa5d4b9
github username <github email address>
2018-03-28 17:52:00 -0400

    added video to welcome page

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -1,13 +1,11 @@
 import React from 'react'
+import ReactPlayer from 'react-player'
 
 const Welcome = () => {
   return(
       <div>
         <h1>Welcome to Musician Finder</h1>
-        <video class='myVideo' autoplay muted loop
-              src='../files/Ma-Vibes.mp4'
-              type="video/mp4">
-        </video>
+        <ReactPlayer style={{margin:'0 auto'}} width='60%' height='60%' url='https://storage.googleapis.com/coverr-main/mp4/Ma-Vibes.mp4' playing  />
       </div>
   )
 }
