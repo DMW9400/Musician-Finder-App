f4750365c027eb29ed6c8e047daf6af8f67f983e
github username <github email address>
2018-07-05 14:30:00 -0400

    restored picture

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -5,7 +5,7 @@ const Welcome = () => {
   return(
       <div>
         <h1 style={{marginTop:'90px', marginBottom:'80px'}}>Welcome to Musician Finder</h1>
-        {/* <ReactPlayer style={{margin:'0 auto'}} width='60%' height='60%' url='https://storage.googleapis.com/coverr-main/mp4/Ma-Vibes.mp4' playing  /> */}
+        <ReactPlayer style={{margin:'0 auto'}} width='60%' height='60%' url='https://storage.googleapis.com/coverr-main/mp4/Ma-Vibes.mp4' playing  />
       </div>
   )
 }
