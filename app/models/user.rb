584d0cf05bdbb19c00c0047e5d9503646e070728
github username <github email address>
2018-07-11 21:32:56 -0400

    added username uniqueness validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   has_many :sent_messages, class_name: "Message", foreign_key: "sender_id"
   has_many :received_messages, class_name: "Message", foreign_key: "recipient_id"
 
+  validates :name, uniqueness: true
 
   has_secure_password
 
