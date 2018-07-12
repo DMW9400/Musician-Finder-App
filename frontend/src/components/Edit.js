c08cf3cbc1da034ffe900d9445fc3ebcd0936df8
github username <github email address>
2018-07-12 16:28:46 -0400

    beginning to implement user editing

---
+++ frontend/src/components/Edit.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import {connect} from 'react-redux '
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
+import TextField from 'material-ui/TextField';
+import FlatButton from 'material-ui/FlatButton';
+import { Link } from 'react-router-dom'
+import { loginUser } from '../actions'
